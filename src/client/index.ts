cc73ca69df1ed10c0b044bf56b054b0863eea846
gooneraki <hayiate4@hotmail.com>
2025-10-31 12:22:12 +0200

    feat: implement transient queue declaration and binding in RabbitMQ client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,29 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind } from "../internal/pubsub/pubsub.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril client...");
+
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Peril game client connected to RabbitMQ!");
+
+  const username = await clientWelcome();
+
+  // Declare and bind a transient queue for receiving pause messages
+  const [channel, queue] = await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    `${PauseKey}.${username}`,
+    PauseKey,
+    "TRANSIENT"
+  );
+  console.log(
+    `Queue ${queue.queue} declared and bound to ${ExchangePerilDirect} exchange`
+  );
 }
 
 main().catch((err) => {
--- src/internal/pubsub/pubsub.ts
+++ src/internal/pubsub/pubsub.ts
@@ -1,4 +1,6 @@
-import type { ConfirmChannel } from "amqplib";
+import type { ConfirmChannel, ChannelModel, Channel, Replies } from "amqplib";
+
+type SimpleQueueType = "DURABLE" | "TRANSIENT";
 
 export async function publishJSON<T>(
   ch: ConfirmChannel,
@@ -13,4 +15,25 @@ export async function publishJSON<T>(
   await ch.publish(exchange, routingKey, jsonBytes, {
     contentType: "application/json",
   });
+}
+
+export async function declareAndBind(
+  conn: ChannelModel,
+  exchange: string,
+  queueName: string,
+  key: string,
+  queueType: SimpleQueueType
+): Promise<[Channel, Replies.AssertQueue]> {
+  const channel = await conn.createConfirmChannel();
+  console.log("Confirm channel created.");
+
+  const queue = await channel.assertQueue(queueName, {
+    durable: queueType === "DURABLE",
+    autoDelete: queueType === "TRANSIENT",
+    exclusive: queueType === "TRANSIENT",
+  });
+
+  await channel.bindQueue(queue.queue, exchange, key);
+
+  return [channel, queue];
 }
