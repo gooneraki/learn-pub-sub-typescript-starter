121a46e49e6017389191a705e12de4335fac090c
gooneraki <hayiate4@hotmail.com>
2025-11-04 13:25:40 +0200

    add dead-letter exchange configuration in declareAndBind function

--- src/internal/pubsub/pubsub.ts
+++ src/internal/pubsub/pubsub.ts
@@ -39,6 +39,9 @@ export async function declareAndBind(
     durable: queueType === SimpleQueueType.DURABLE,
     autoDelete: queueType === SimpleQueueType.TRANSIENT,
     exclusive: queueType === SimpleQueueType.TRANSIENT,
+    arguments: {
+      "x-dead-letter-exchange": "peril_dlx",
+    },
   });
 
   await channel.bindQueue(queue.queue, exchange, key);
